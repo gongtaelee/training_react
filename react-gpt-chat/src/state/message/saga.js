a897860c3db660f04f3556315e1fdc6ef4fcfc88
leewontae <leewt2@asianaidt.com>
2023-11-27 18:29:32 +0900

    추가 수정

--- react-gpt-chat/src/state/message/saga.js
+++ react-gpt-chat/src/state/message/saga.js
@@ -3,6 +3,10 @@ import { actions, types } from './index';
 import { callApi } from '../../api';
 
 function* fetchUserMessage(action){
+
+    yield put(actions.pendingMessageState(true)); // 리덕스 액션을 발생 시킴
+    yield put(actions.addMessage(action.message));
+
     const message = action.message.content
     const { isSuccess, data } = yield call(callApi, {
         url: '',
@@ -10,12 +14,10 @@ function* fetchUserMessage(action){
     });
 
     if (isSuccess && data){
-        yield put(actions.setValue(''))
+        // 서버에서 받은 데이터 파싱 필요
+        yield put(actions.addMessage(action.message));
     }
 
-    yield put(actions.pendingMessageState(true)); // 리덕스 액션을 발생 시킴
-    yield put(actions.addMessage(action.message));
-    // yield call(callApiLike);
     yield put(actions.pendingMessageState(false));
 }
 
